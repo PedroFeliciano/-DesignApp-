dce67a5288f67ae013b01fd4620447aac59fcdb4
PedroFeliciano <pedrofeliciano647@gmail.com>
2022-08-30 18:02:24 -0300

    PRIMEIRO

---
+++ HousyTest/HousyTest/Cadastro do usuario.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HousyTest
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Cadastro_do_usuario : ContentPage
+    {
+        public Cadastro_do_usuario()
+        {
+            InitializeComponent();
+        }
+    }
+}
